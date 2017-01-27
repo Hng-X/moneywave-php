279058304a60fcef64f1c4e19065a99affa125ae
Shalvah <diakon.ng@gmail.com>
2017-01-27 13:51:12 +0100

    Add list of banks

---
+++ src/Bank.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: J
+ * Date: 27/01/2017
+ * Time: 13:28
+ */
+
+namespace Moneywave;
+
+
+class Bank {
+    /**
+FIRST CITY MONUMENT BANK PLC
+     * 
+     */
+    static $FCMB = "214";
+
+
+    /**
+    UNITY BANK PLC
+     *
+     */
+static $UNITY = "215";
+    
+    /**
+STANBIC IBTC BANK PLC
+ *
+ */
+static $STANBIC_IBTC  = "221";
+
+    /**
+    STERLING BANK PLC
+     *
+     */
+static $STERLING = "232";
+
+    /**
+    STANBIC Mobile PLC
+     *
+     */
+static $STANBIC_MOBILE= "304";
+
+    /**
+    PAYCOM
+     *
+     */
+static $PAYCOM = "305";
+
+    /**
+    ECOBANK MOBILE
+     *
+     */
+static $ECOBANK_MOBILE = "307";
+
+    /**
+FBN MOBILE
+ *
+ */
+static $FBN_MOBILE = "309";
+
+    /**
+    PARKWAY
+     *
+     */
+static $Parkway = "311";
+
+    /**
+    GTBank Mobile Money
+     *
+     */
+static $GTBANK_MOBILE= "315";
+
+    /**
+    ZENITH Mobile
+     *
+     */
+static $ZENITH_MOBILE = "322";
+
+    /**
+    ACCESS Mobile
+     *
+     */
+static $ACCESS_MOBILE = "323";
+
+    /**
+    Aso Savings and Loans
+     *
+     */
+static $ASO = "401";
+
+    /**
+    ACCESS BANK NIGERIA
+     *
+     */
+static $ACCESS = "044";
+
+    /**
+    AFRIBANK NIGERIA PLC
+     *
+     */
+static $AFRIBANK= "014";
+
+    /**
+    DIAMOND BANK PLC
+     *
+     */
+static $DIAMOND = "063";
+
+    /**
+    ECOBANK NIGERIA PLC
+     *
+     */
+static $ECOBANK = "050";
+
+    /**
+    ENTERPRISE BANK LIMITED
+     *
+     */
+static $ENTERPRISE  = "084";
+
+    /**
+    FIDELITY BANK PLC
+     *
+     */
+static $FIDELITY = "070";
+
+
+    /**
+    FIRST BANK PLC
+     *
+     */
+static $FIRST= "011";
+
+/**
+GTBANK PLC
+ *
+ */
+static $GTBANK= "058";
+
+/**
+HERITAGE BANK
+ *
+ */
+static $HERITAGE = "030";
+
+/**
+KEYSTONE BANK PLC
+ *
+ */
+static $KEYSTONE = "082";
+
+
+/**
+SKYE BANK PLC
+ *
+ */
+static $SKYE = "076";
+
+/**
+STANDARD CHARTERED BANK NIGERIA LIMITED
+ *
+ */
+static $STANDARD_CHARTERED= "068";
+
+
+/**
+UNION BANK OF NIGERIA PLC
+ *
+ */
+static $UNION = "032";
+
+   /**
+   UNITED BANK FOR AFRICA PLC
+    *
+    */
+static $UBA = "033";
+
+     /**
+     WEMA BANK PLC
+      *
+      */
+static $WEMA = "035";
+
+    /**
+    ZENITH BANK PLC
+     *
+     */
+static $ZENITH = "057";
+
+}
