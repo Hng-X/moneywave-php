d45b467749b702190225fc2f634838226e60b80f
Shalvah <diakon.ng@gmail.com>
2017-01-28 10:39:21 +0100

    Add esception handling

--- src/MoneywaveApiClient.php
+++ src/MoneywaveApiClient.php
@@ -3,6 +3,8 @@
 namespace Moneywave;
 
 
+use GuzzleHttp\Exception\RequestException;
+
 abstract class MoneywaveApiClient {
 
     protected $moneywave;
@@ -41,13 +43,19 @@ abstract class MoneywaveApiClient {
 
     public function dispatch($method = "POST")
     {
+        try {
         $result = $this->moneywave->request($method, $this->url, array(
             "form_params" => $this->data,
             "headers" => ["Authorization" => $this->moneywave->getToken()]
         ));
-        $this->response = json_decode($result->getBody(), true);
-        $this->responseCode = $result->getStatusCode();
-        $this->status = $this->response["status"];
+            $this->response = json_decode($result->getBody(), true);
+            $this->responseCode = $result->getStatusCode();
+            $this->status = $this->response["status"];
+        } catch (RequestException $e) {
+            $this->responseCode = $e->getCode();
+            $this->response = $e->getMessage();
+            $this->status = $e->getMessage();
+        }
     }
 
     public function getResponse()
