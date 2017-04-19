c2b5e7d025b432ea33cd2403cd4a3a9ed3dce7f6
Shalvah <diakon.ng@gmail.com>
2017-04-19 14:46:27 +0100

    Fix syntax error

--- src/Bank.php
+++ src/Bank.php
@@ -9,178 +9,178 @@ class Bank
      * FIRST CITY MONUMENT BANK PLC
      *
      */
-    static const FCMB = "214";
+    const FCMB = "214";
 
 
     /**
      * UNITY BANK PLC
      *
      */
-    static const UNITY = "215";
+    const UNITY = "215";
 
     /**
      * STANBIC IBTC BANK PLC
      *
      */
-    static const STANBIC_IBTC = "221";
+    const STANBIC_IBTC = "221";
 
     /**
      * STERLING BANK PLC
      *
      */
-    static const STERLING = "232";
+    const STERLING = "232";
 
     /**
      * STANBIC Mobile PLC
      *
      */
-    static const STANBIC_MOBILE = "304";
+    const STANBIC_MOBILE = "304";
 
     /**
      * PAYCOM
      *
      */
-    static const PAYCOM = "305";
+    const PAYCOM = "305";
 
     /**
      * ECOBANK MOBILE
      *
      */
-    static const ECOBANK_MOBILE = "307";
+    const ECOBANK_MOBILE = "307";
 
     /**
      * FBN MOBILE
      *
      */
-    static const FBN_MOBILE = "309";
+    const FBN_MOBILE = "309";
 
     /**
      * PARKWAY
      *
      */
-    static const PARKWAY = "311";
+    const PARKWAY = "311";
 
     /**
      * GTBank Mobile Money
      *
      */
-    static const GTBANK_MOBILE = "315";
+    const GTBANK_MOBILE = "315";
 
     /**
      * ZENITH Mobile
      *
      */
-    static const ZENITH_MOBILE = "322";
+    const ZENITH_MOBILE = "322";
 
     /**
      * ACCESS Mobile
      *
      */
-    static const ACCESS_MOBILE = "323";
+    const ACCESS_MOBILE = "323";
 
     /**
      * Aso Savings and Loans
      *
      */
-    static const ASO = "401";
+    const ASO = "401";
 
     /**
      * ACCESS BANK NIGERIA
      *
      */
-    static const ACCESS = "044";
+    const ACCESS = "044";
 
     /**
      * AFRIBANK NIGERIA PLC
      *
      */
-    static const AFRIBANK = "014";
+    const AFRIBANK = "014";
 
     /**
      * DIAMOND BANK PLC
      *
      */
-    static const DIAMOND = "063";
+    const DIAMOND = "063";
 
     /**
      * ECOBANK NIGERIA PLC
      *
      */
-    static const ECOBANK = "050";
+    const ECOBANK = "050";
 
     /**
      * ENTERPRISE BANK LIMITED
      *
      */
-    static const ENTERPRISE = "084";
+    const ENTERPRISE = "084";
 
     /**
      * FIDELITY BANK PLC
      *
      */
-    static const FIDELITY = "070";
+    const FIDELITY = "070";
 
 
     /**
      * FIRST BANK PLC
      *
      */
-    static const FIRST = "011";
+    const FIRST = "011";
 
     /**
      * GTBANK PLC
      *
      */
-    static const GTBANK = "058";
+    const GTBANK = "058";
 
     /**
      * HERITAGE BANK
      *
      */
-    static const HERITAGE = "030";
+    const HERITAGE = "030";
 
     /**
      * KEYSTONE BANK PLC
      *
      */
-    static const KEYSTONE = "082";
+    const KEYSTONE = "082";
 
 
     /**
      * SKYE BANK PLC
      *
      */
-    static const SKYE = "076";
+    const SKYE = "076";
 
     /**
      * STANDARD CHARTERED BANK NIGERIA LIMITED
      *
      */
-    static const STANDARD_CHARTERED = "068";
+    const STANDARD_CHARTERED = "068";
 
 
     /**
      * UNION BANK OF NIGERIA PLC
      *
      */
-    static const UNION = "032";
+    const UNION = "032";
 
     /**
      * UNITED BANK FOR AFRICA PLC
      *
      */
-    static const UBA = "033";
+    const UBA = "033";
 
     /**
      * WEMA BANK PLC
      *
      */
-    static const WEMA = "035";
+    const WEMA = "035";
 
     /**
      * ZENITH BANK PLC
      *
      */
-    static const ZENITH = "057";
+    const ZENITH = "057";
 
 }
---
+++ src/Resources/TokenizeCard.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Moneywave\Resources;
+
+
+use Moneywave\Moneywave;
+
+class TokenizeCard  extends Resource{
+
+    protected $url="/v1/transfer/charge/tokenize/card";
+
+    public function __construct(Moneywave $mw)
+    {
+        parent::__construct($mw);
+        $this->data = array(
+            "card_no" => "",
+            "expiry_year" => "",
+            "expiry_month" => "",
+            "cvv" => "",
+        );
+    }
+
+    public function getCardToken()
+    {
+        return $this->getResponse()["data"]["cardToken"];
+    }
+}
